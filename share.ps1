e20f684dc87058ce1486c63af8bc489d27c97b59
zackpdm <pduyminh0903@gmail.com>
2026-04-08 23:26:25 +0700

    abc

--- share.ps1
+++
@@ -1,8 +0,0 @@
-# Chạy dự án và public công khai
-Write-Host "Đang khởi động Server và tạo link công khai..." -ForegroundColor Cyan
-
-# Khởi động live-server ở background (nếu chưa chạy)
-Start-Process powershell -ArgumentList "npx live-server --port=8080" -WindowStyle Hidden
-
-Write-Host "Đang tạo đường hầm (tunnel)..." -ForegroundColor Yellow
-npx localtunnel --port 8080
